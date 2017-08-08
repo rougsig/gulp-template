abf2977f95f1fc5a266ae15460c5b7d4a334413e
rougsig <rougsig@gmail.com>
2017-08-08 19:33:59 +0300

    node_modules init

---
+++ init.bat
@@ -0,0 +1,1 @@
+npm link gulp gulp-watch browser-sync gulp-pug gulp-postcss gulp-stylus autoprefixer rucksack-css poststylus postcss-image-sizes postcss-pxtorem postcss-short css-mqpacker postcss-clean postcss-sprites postcss-assets gulp-uglify gulp-rigger gulp-plumber gulp-imagemin gulp-rename
